c4d6f1dad28139725ba2a155c31035312190ca91
khai96_ <hvksmr1996@gmail.com>
2016-08-14 16:46:22 +0700

    add a few lines

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 'use strict'
+
+const {CompositeDisposable} = require('atom')
+
+let subscriptions = null
+
+function activate () {
+  subscriptions = new CompositeDisposable()
+}
+
+function deactivate () {
+  subscriptions.dispose()
+  subscriptions = null
+}
+
+module.exports = {
+  activate, deactivate,
+  get subscriptions () {
+    return subscriptions
+  }
+}
