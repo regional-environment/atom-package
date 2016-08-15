2146d4d8f5589a3a74a07230682e71a1ccacb9da
khai96_ <hvksmr1996@gmail.com>
2016-08-15 12:06:48 +0700

    update index

--- index.js
+++ index.js
@@ -3,19 +3,25 @@
 const {CompositeDisposable} = require('atom')
 
 let subscriptions = null
+let api = null
 
 function activate () {
   subscriptions = new CompositeDisposable()
+  api = require('./api.js').init({subscriptions})
 }
 
 function deactivate () {
   subscriptions.dispose()
   subscriptions = null
+  api = null
 }
 
 module.exports = {
   activate, deactivate,
   get subscriptions () {
     return subscriptions
+  },
+  get api () {
+    return api
   }
 }
