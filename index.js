8966f09bce310ef5c0976299fb203935693e573a
khai96_ <hvksmr1996@gmail.com>
2016-08-29 09:51:49 +0700

    pre-implement loader

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const {CompositeDisposable} = require('atom')
+const loader = require('./loader.js')
 
 let subscriptions = null
 let api = null
@@ -8,12 +9,14 @@ let api = null
 function activate () {
   subscriptions = new CompositeDisposable()
   api = require('./api.js').init({subscriptions})
+  subscriptions.add(loader.activate(api))
 }
 
 function deactivate () {
   subscriptions.dispose()
   subscriptions = null
   api = null
+  loader.deactivate(api)
 }
 
 module.exports = {
