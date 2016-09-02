2e2f756a98a4f37a19db8b9abaf53b46c5a8ca32
khai96_ <hvksmr1996@gmail.com>
2016-09-02 19:21:49 +0700

    update

--- loader.js
+++ loader.js
@@ -1,15 +1,48 @@
 'use strict'
 
+const {readdir, readFile, statSync} = require('fs')
+const {arch, platform} = require('process')
 const {CompositeDisposable} = require('atom')
+const regenv = require('regional-environment')
+const SymMap = require('./lib/sym-map.js')
 
-function activate ({on}) {
+const OPTIONAL_ENV_CONFIG = ['atom', arch, platform]
+
+const envcfgdesc = required => ({
+  required: ['config', ...required],
+  optional: OPTIONAL_ENV_CONFIG,
+  __proto__: null
+})
+
+const items = (dirname, required) =>
+  regenv.loadItems(dirname, envcfgdesc(required))
+
+function activate ({on, notifications}) {
+  const fileplugset = new Set()
+  const projplugset = new Set()
+  const fileplugmap = new SymMap()
+  const projplugmap = new SymMap()
   return new CompositeDisposable(
     on('enter-file', filedesc => {}),
     on('leave-file', filedesc => {}),
     on('open-file', filedesc => {}),
     on('close-file', filedesc => {}),
     on('change-projects', projectdescset => {}),
-    on('add-project', projectdesc => {}),
+    on('add-project', projectdesc => {
+      const {dirname} = projectdesc
+      readdir(dirname, (error, list) => {
+        if (error) {
+          notifications.addError('Load project failed', {
+            detail: `Can't access "${dirname}" as a directory`
+          })
+          return
+        }
+        for (const plugin of loadEnvConfig(dirname)) {
+          projplugset.add(plugin)
+          projplugmap.set(plugin, dirname)
+        }
+      })
+    }),
     on('remove-project', projectdesc => {}),
     on('atom/enter-tab', editor => {}),
     on('atom/leave-tab', editor => {}),
@@ -21,7 +54,19 @@ function activate ({on}) {
 
 function deactivate () {}
 
+function enable () {}
+
+function disable () {}
+
+function loadEnvConfig (dirname) {
+  items(dirname, ['js']) // do something more...
+  items(dirname, ['json']) // do something more...
+}
+
 module.exports = {
   activate,
-  deactivate
+  deactivate,
+  enable,
+  disable,
+  loadEnvConfig
 }
